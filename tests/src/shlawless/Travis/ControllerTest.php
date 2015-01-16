7471e6d253dbe78f4f2b79f644258204caed41ff
Shaun Lawless <shlawless@paddypower.com>
2015-01-16 11:29:21 +0000

    develop branch start work

--- tests/src/shlawless/Travis/ControllerTest.php
+++ tests/src/shlawless/Travis/ControllerTest.php
@@ -17,4 +17,20 @@ class ControllerTest extends PHPUnit_Framework_TestCase
 
         $this->assertSame('{"format":"xml"}', $controller->indexAction($request));
     }
+	
+	/**
+     * @test
+     * @covers shlawless\Travis\Controller::indexAction
+     */
+    public function indexActionFails()
+    {
+        $request = $this->getMockBuilder('\\Symfony\\Component\\HttpFoundation\\Request')->disableOriginalConstructor()->getMock();
+        $request->expects($this->any())
+            ->method('get')
+            ->will($this->returnValue('xmls'));
+
+        $controller = new shlawless\Travis\Controller;
+
+        $this->assertNotSame('{"format":"xml"}', $controller->indexAction($request));
+    }
 }
